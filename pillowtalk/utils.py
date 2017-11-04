1120410c3db0801f25baf3465b986dee26dedfa6
jvrana <justin.vrana@gmail.com>
2017-11-04 11:38:27 -0700

    refactored init utils

--- pillowtalk/utils.py
+++ pillowtalk/utils.py
@@ -6,20 +6,21 @@ def validate_init(fxn):
 
     def wrapped(*args, **kwargs):
         self = args[0]
+        classname = self.__class__.__name__
         args = args[1:]
         fields = self.__class__.FIELDS
         field_dict = {}
         if len(args) > len(fields):
             raise TypeError("Expected {0} arguments but got {1} for {2}.__init__".format(
-                    len(fields), len(args), self.__class__.__name__))
+                    len(fields), len(args), classname))
         for key, val in kwargs.items():
             if key in field_dict:
-                raise PillowtalkInitializerError("{0} got multiple values for {1}".format(self.__class__.__name__, key))
+                raise PillowtalkInitializerError("{0} got multiple values for {1}".format(classname, key))
             else:
                 field_dict[key] = val
         for field in fields:
             if field not in field_dict.keys():
-                raise PillowtalkInitializerError("{0} is missing argument {1}".format(self.__class__.__name__, field))
+                raise PillowtalkInitializerError("{0} is missing argument {1}".format(classname, field))
         return fxn(self, (), **field_dict)
 
     return wrapped
