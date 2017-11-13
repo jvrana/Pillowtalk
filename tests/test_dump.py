7664942951aa4e96c70084464963ab95ff1bcaf9
jvrana <justin.vrana@gmail.com>
2017-11-13 12:22:52 -0800

    fixed global changes to field ops upon dump
    field options no longer remember when an instance was dumped

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -43,3 +43,49 @@ def test_load_only():
     d = u.dump()
     assert d == {"id": 5, "field1": 1}
 
+
+def test_load_dump_repeat():
+    """Test loading a model with ignored fields. The model is expected to NOT have an id or field1, but
+    have a name"""
+
+    @add_schema
+    class MyModel(PillowtalkBase):
+        class Fields:
+            load_all = True
+
+    original_data = {"id": 5, "name": "Jill", "field1": 1}
+
+    ## load/dump cycle 1
+
+    # check load
+    u = MyModel.load(original_data)
+    assert u.id == 5
+    assert u.field1 == 1
+    assert u.name == "Jill"
+
+    # check dump
+    d = u.dump()
+    assert d == original_data
+
+    # check instance again
+    assert u.id == 5
+    assert u.field1 == 1
+    assert u.name == "Jill"
+
+
+    ## load/dump cycle 2
+
+    # check load
+    u = MyModel.load(original_data)
+    assert u.id == 5
+    assert u.field1 == 1
+    assert u.name == "Jill"
+
+    # check dump
+    d = u.dump()
+    assert d == original_data
+
+    # check instance again
+    assert u.id == 5
+    assert u.field1 == 1
+    assert u.name == "Jill"
